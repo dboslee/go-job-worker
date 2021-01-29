167948fe360cd94c0e232120109abdefc6580639
dboslee <dboslee@gmail.com>
2021-01-29 08:36:29 -0500

    Change tempfile to tempdir so we don't need to open/close file

--- pkg/core/output.go
+++ pkg/core/output.go
@@ -8,39 +8,37 @@ import (
 
 // OutputBuffer provides
 type OutputBuffer struct {
-	f *os.File
+	name string
 }
 
 // NewOutputBuffer creates a OutputBuffer instance
 func NewOutputBuffer() (*OutputBuffer, error) {
 
-	// TODO: Swap out tempfile for a permanent solution
+	// TODO: Swap out tempdir for a permanent solution
 	// This is easier to setup/cleanup
-	f, err := ioutil.TempFile("", "job-worker-output-*")
+	dir, err := ioutil.TempDir("", "job-worker-output-*")
+	if err != nil {
+		return nil, err
+	}
+	f, err := os.Create(dir + "/log")
 	if err != nil {
 		return nil, err
 	}
 
-	// TempFile opens the file by default so we will close immediately
-	err = f.Close()
-	if err != nil {
-		return nil, err
-	}
-
-	return &OutputBuffer{f: f}, nil
+	return &OutputBuffer{name: f.Name()}, nil
 }
 
 // NewReader opens the file read only
 func (o *OutputBuffer) NewReader() (io.ReadCloser, error) {
-	return os.Open(o.f.Name())
+	return os.Open(o.name)
 }
 
 // NewWriter opens the file write only
 func (o *OutputBuffer) NewWriter() (io.WriteCloser, error) {
-	return os.OpenFile(o.f.Name(), os.O_WRONLY, os.ModeAppend)
+	return os.OpenFile(o.name, os.O_WRONLY, os.ModeAppend)
 }
 
 // Remove the file
 func (o *OutputBuffer) Remove() error {
-	return os.Remove(o.f.Name())
+	return os.Remove(o.name)
 }
