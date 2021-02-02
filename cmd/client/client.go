d529b07973d3b468d137ea3a2d0b47b75dab482e
dboslee <dboslee@gmail.com>
2021-02-01 20:59:55 -0500

    Add main package to bootstrap cli client

---
+++ cmd/client/client.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/dboslee/job-worker/pkg/api/proto"
+	"github.com/dboslee/job-worker/pkg/auth"
+	"github.com/dboslee/job-worker/pkg/cli"
+
+	"google.golang.org/grpc"
+)
+
+func init() {
+	log.SetFlags(0)
+}
+
+// Bootstrap client
+func main() {
+	// TODO: Make certs and port configurable
+	tlsCreds, err := auth.LoadClientTLS("certs/client1.pem", "certs/client1.key", "certs/ca.pem")
+	if err != nil {
+		log.Print(err)
+		os.Exit(1)
+	}
+	conn, err := grpc.Dial("0.0.0.0:8888", grpc.WithTransportCredentials(tlsCreds))
+	if err != nil {
+		log.Print(err)
+		os.Exit(1)
+	}
+
+	ctx := context.Background()
+	jobServiceClient := proto.NewJobServiceClient(conn)
+	cliClient := cli.NewClient(ctx, jobServiceClient)
+
+	code, err := cliClient.HandleArgs(os.Args)
+	if err != nil {
+		log.Print(err)
+	}
+	os.Exit(code)
+}
